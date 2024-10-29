053a3e99a3ca2ae99608e0cee20ce53568d55109
DanielPoprawski <danielbpoprawski@gmail.com>
2024-10-29 19:49:29 -0400

    Fixed some css

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -3,46 +3,35 @@ import { useState } from "react";
 export default function Sidebar() {
         const size = "24px";
         const [hidden, setHidden] = useState(true);
+        const [style, setStyle] = useState({ display: "none" });
 
         function toggleHidden() {
                 setHidden(!hidden);
+                if (hidden) {
+                        setStyle({ display: "block" });
+                } else {
+                        setStyle({ display: "none" });
+                }
         }
 
         return (
                 <div className="sidebar">
-                        <a href="#menu">
-                                <img
-                                        src="../src/assets/icons/menu-svgrepo-com.svg"
-                                        width={size}
-                                        height={size}
-                                        onClick={toggleHidden}
-                                />
+                        <a href="#menu" onClick={toggleHidden}>
+                                <img src="../src/assets/icons/menu-svgrepo-com.svg" width={size} height={size} />
+                        </a>
+                        <a href="#home" style={style}>
+                                <img src="../src/assets/icons/home-svgrepo-com.svg" width={size} height={size} />
                         </a>
 
-                        <a href="#home">
-                                <img
-                                        src="../src/assets/icons/home-svgrepo-com.svg"
-                                        width={size}
-                                        height={size}
-                                        hidden={hidden}
-                                />
+                        <a href="#settings" style={style}>
+                                <img src="../src/assets/icons/gear-svgrepo-com.svg" width={size} height={size} />
                         </a>
 
-                        <a href="#settings">
-                                <img
-                                        src="../src/assets/icons/gear-svgrepo-com.svg"
-                                        width={size}
-                                        height={size}
-                                        hidden={hidden}
-                                />
-                        </a>
-
-                        <a href="#learn">
+                        <a href="#learn" style={style}>
                                 <img
                                         src="../src/assets/icons/chevron-right-svgrepo-com.svg"
                                         width={size}
                                         height={size}
-                                        hidden={hidden}
                                 />
                         </a>
                 </div>
