c461b9e94998dc8c568214c1ef3aa2cd2d9dde0b
DanielPoprawski <danielbpoprawski@gmail.com>
2025-06-06 21:55:25 -0400

    Updated states

--- src/pages/Memorize.jsx
+++ src/pages/Memorize.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect } from "react";
 import { setTitle } from "../main.jsx";
-import { Loader2, Loader2Icon } from "lucide-react";
+import { Loader2 } from "lucide-react";
 import { useParams } from "react-router-dom";
 
 const fileDirectory = import.meta.glob("/public/poems/*.json");
@@ -16,13 +16,18 @@ export default function Memorize() {
 
       const [isLoading, setLoading] = useState(true);
 
-      const [selectedPoem, updatePoem] = useState(null);
+      const [selectedPoem, updatePoem] = useState("");
       const [body, setBody] = useState(null);
 
+      const [loadedData, setLoadedData] = useState([]);
+
       useEffect(() => {
-            loadData().then(() => {
+            const loadPoems = async () => {
+                  const data = await loadData();
+                  setLoadedData(data);
                   setLoading(false);
-            });
+            };
+            loadPoems();
       }, [params]);
 
       const handleFocus = () => {
@@ -35,16 +40,13 @@ export default function Memorize() {
       };
 
       async function loadData() {
-            const allData = {};
+            const allData = [];
             for (const path in fileDirectory) {
                   const module = await fileDirectory[path]();
                   const fileName = path.split("/").pop().replace(".json", "");
                   allData[fileName] = module.default;
             }
-            const filteredData = Object.entries(allData).filter((f) => {
-                  return f[0] === params;
-            });
-            console.log(filteredData[0][1]);
+            return allData;
       }
 
       useEffect(() => {
@@ -62,25 +64,12 @@ export default function Memorize() {
             };
       }, [focusRef.current]);
 
-      //TODO: Fix Splitter so that it highlights wrong letters as red
-      //TODO: Add settings so only the next couple words are visible
-
       function clearInput() {
             setInput("");
             setBody(paragraph);
       }
 
       function handleEvent(event) {
-            //	if (keys.includes(event.key)) {
-            //		setInput(input + event.key);
-            //		setBody(body.substring(1));
-            //	} else if (event.key === "Backspace") {
-            //		event.preventDefault();
-            //		setInput(input.slice(0, -1));
-            //		setBody(paragraph.substring(input.slice(0, -1).length));
-            //	} else {
-            //		console.log(event.key);
-            //	}
             if (event.key === "Backspace" || event.key === "Escape") {
                   event.preventDefault();
             }
@@ -94,47 +83,53 @@ export default function Memorize() {
             }
       }
 
-      function Splitter() {
-            return (
-                  <div className="animate-blink inline">
-                        {input.split("").map((element, index) => {
-                              return (
-                                    <span key={index} className="text-black">
-                                          {element}
-                                    </span>
-                              );
-                        })}
-                  </div>
-            );
-      }
-
       return (
             <div className="flex-1 p-8 container mx-auto">
-                  <div className="text-4xl font-bold mb-4 p-8 flex justify-between">
-                        {isFocused ? "Start typing..." : "Wait..."}
-                        <button
-                              type="button"
-                              onClick={clearInput}
-                              className="cursor-pointer bg-red-500 hover:bg-red-700 text-amber-100 text-lg font-lg py-2 px-4 rounded transition-colors border-red-600 border-2"
-                        >
-                              Clear
-                        </button>
-                  </div>
+                  {isLoading && <Loader2 className="animate-spin mx-auto justify-center h-screen w-24 text-gray-600" />}
 
-                  {isLoading ? (
-                        <Loader2 className="animate-spin" />
-                  ) : (
-                        <h2
-                              tabIndex={0}
-                              className="typing text-xl font-medium font-mono relative text-gray-300 focus:outline-none p-4 text-center"
-                              onKeyDown={handleEvent}
-                              ref={focusRef}
-                        >
-                              <Splitter />
+                  {!isLoading && (
+                        <>
+                              {selectedPoem && selectedPoem != "" && (
+                                    <>
+                                          <div className="text-4xl font-bold mb-4 p-8 flex justify-between">
+                                                {isFocused ? "Start typing..." : "Wait..."}
+                                                <button
+                                                      type="button"
+                                                      onClick={clearInput}
+                                                      className="cursor-pointer bg-red-500 hover:bg-red-700 text-amber-100 text-lg font-lg py-2 px-4 rounded transition-colors border-red-600 border-2"
+                                                >
+                                                      Clear
+                                                </button>
+                                          </div>
+                                          <h2
+                                                tabIndex={0}
+                                                className="typing text-xl font-medium font-mono relative text-gray-300 focus:outline-none p-4 text-center"
+                                                onKeyDown={handleEvent}
+                                                ref={focusRef}
+                                          >
+                                                <div className="animate-blink inline">
+                                                      {input.split("").map((element, index) => {
+                                                            return (
+                                                                  <span key={index} className="text-black">
+                                                                        {element}
+                                                                  </span>
+                                                            );
+                                                      })}
+                                                </div>
 
-                              {body}
-                              <br />
-                        </h2>
+                                                {body}
+                                                <br />
+                                          </h2>
+                                    </>
+                              )}
+                              {!selectedPoem ||
+                                    (selectedPoem == "" && (
+                                          <>
+                                                <div>No Poem Selected</div>
+                                                <div>Select one of the available poems:</div>
+                                          </>
+                                    ))}
+                        </>
                   )}
             </div>
       );
